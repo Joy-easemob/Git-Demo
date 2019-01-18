62c1e431b904b8cdebec8b55ac31dd863140cfe2
joy-easemob <joy-easemob@easemob.com>
2019-01-18 21:28:58 +0800

    我是joy的第四次提交

--- src/com/dbstudy/git/Hello.java
+++ src/com/dbstudy/git/Hello.java
@@ -6,6 +6,6 @@ package com.dbstudy.git;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("我是joy的第三次提交，我是为了验证idea的合并冲突，在我之前会有人切换到master分支修改代码。。。");
+        System.out.println("我是joy的第四次提交，我是为了验证idea的合并冲突，在我之前会有人切换到master分支修改代码，我发誓我不知道远程仓库被提交了修改。。。");
     }
 }
